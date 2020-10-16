4d192a9d21badb234fedecc7fe880f728ecccb0b
Briley Burks <brileyburks@BCs-MBP-4.glasgow-ky.com>
2020-10-16 10:04:09 -0500

    Completed variables exercise

---
+++ variablesExercise.js
@@ -0,0 +1,22 @@
+// 1
+let myAge = 17;
+
+// 2
+myAge = 22;
+
+// 3
+// myAge = myAge + 6;
+myAge += 6;
+
+// 4
+myAge--;
+
+// 5
+myAge = "None of your business";
+
+// 6
+console.log(myAge);
+
+// 7
+let blank;
+blank = null;
